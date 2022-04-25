b565d606adceab2a986514e2e137c611c60c636a
Precious-Usanga <precioususanga001@gmail.com>
2022-04-25 11:06:37 +0100

    updated prod env data for deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  API_KEY: '95533401eab941bdb2695fe6ac8f2ad4',
+  BASE_URL: 'https://newsapi.org',
+  BASE_URL_VERSION: {
+    'v2': 'v2',
+  }
 };
